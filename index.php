f7c0742776696efe353a202cc794812fc670d086
EmanuelaPau <uapame@gmail.com>
2023-06-30 15:32:20 +0200

    add name and passwords variable

--- index.php
+++ index.php
@@ -1,2 +1,24 @@
 <?php
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Form</title>
+</head>
+<body>
+    <form action="./server.php" method="POST">
+        <label for="userName">Name</label>
+        <input type="text" name="userName" id="">
+        <label for="userName">Password</label>
+        <input type="password" name="userPassword" id="">
+        <button type="submit">send</button>
+    </form>
+
+    <div>
+
+    </div>
+</body>
+</html>
